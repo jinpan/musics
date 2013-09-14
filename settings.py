4c0b8e27ec2c2d1b809da8192a4482a0844f75e4
Shri Ganeshram <shrig94@gmail.com>
2013-09-14 12:38:18 -0400

    setup stuff rm env

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for musics project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -107,11 +108,11 @@ ROOT_URLCONF = 'urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+TEMPLATE_DIRS = [
+    os.path.join(PACKAGE_ROOT, "templates"),
+]
 
 INSTALLED_APPS = (
     'django.contrib.auth',
--- urls.py
+++ urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import include
 from django.conf.urls import patterns
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
-from musics.core import urls as core_urls
+# from musics.core import urls as core_urls
 
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'core.views.home', name='home'),
+    url(r'^list', TemplateView.as_view(template_name='list.html')),
 
 )
 
-urlpatterns += core_urls.urlpatterns
-
+# urlpatterns += core_urls.urlpatterns
