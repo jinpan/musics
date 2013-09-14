a9d7fdf0c1112ead9c655943b36d5b59a5f5629d
Jin Pan <jinpan@mit.edu>
2013-09-13 21:02:58 -0400

    refactored default format

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'music.views.home', name='home'),
+    url(r'^$', 'core.views.home', name='home'),
     # url(r'^music/', include('music.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
